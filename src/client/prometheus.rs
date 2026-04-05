d4500407febc87f262de6857ece87c0c6ab0fd1f
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-04-05 08:49:58 +0000

    feat: added prometheus client

--- src/client/prometheus.rs
+++ src/client/prometheus.rs
@@ -1,8 +1,52 @@
 use std::time::SystemTime;
+use serde::{Deserialize,Serialize};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
+use std::time::Duration;
 
-/// Calls prometheus api along with client config, query and params
-pub fn run_prometheus(query: String, query_params: PrometheusQueryParams, prometheus_config: PrometheusConfig) {
-    println!("Executing {} with {:?} and config {:?}", query, query_params, prometheus_config);
+#[derive(Debug)]
+pub struct PrometheusError {
+    msg: String
+}
+
+impl fmt::Display for PrometheusError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Something went wrong with Prometheus {0}", self.msg)
+    }
+}
+
+impl Error for PrometheusError {}
+
+impl From<reqwest::Error> for PrometheusError {
+    fn from(err: reqwest::Error) -> Self {
+        PrometheusError {msg: format!("HTTP request failed {}", err) }
+    }
+} 
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PrometheusResponse {
+    status: String,
+    data: PrometheusData
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(tag = "resultType", content = "result", rename_all = "camelCase")]
+pub enum PrometheusData {
+    Vector(Vec<VectorResult>),
+    Matrix(Vec<MatrixResult>)
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct VectorResult {
+    pub metric: HashMap<String, String>,
+    pub value: (f64, String)
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MatrixResult {
+    pub metric: HashMap<String, String>,
+    pub values: Vec<(f64, String)>
 }
 
 #[derive(Debug)]
@@ -28,4 +72,48 @@ impl Default for PrometheusQueryParams {
     fn default() -> Self {
         Self { limit: None, start: None, end: None }
     }
+}
+
+/// Calls prometheus api along with client config, query and params
+pub fn run_prometheus(query: String, query_params: PrometheusQueryParams, prometheus_config: PrometheusConfig) 
+        -> Result<PrometheusResponse, PrometheusError> {
+    // sanity check: 1. ensure query string is non empty
+    if query.trim().is_empty() {
+        return Err(PrometheusError{msg: "Query string cannot be empty".to_string()})
+    }
+    
+    let client = reqwest::blocking::Client::builder()
+        .timeout(Duration::from_secs(prometheus_config.timeout as u64))
+        .build()?;
+
+    let url = format!("{}/api/v1/query",prometheus_config.base_url.trim_end_matches('/'));
+    let mut params = vec![("query".to_string(), query.clone())];
+
+    if let Some(limit) = query_params.limit {
+        params.push(("limit".to_string(), limit.to_string()))
+    }
+
+    if let Some(start_time) = query_params.start {
+        if let Ok(duration) = start_time.duration_since(std::time::UNIX_EPOCH) {
+            params.push(("start".to_string(), duration.as_secs().to_string()));
+        } else {
+            return Err(PrometheusError{msg: format!("Error with time {:?}", start_time)})
+        }
+    }
+
+    if let Some(end_time) = query_params.end {
+        if let Ok(duration) = end_time.duration_since(std::time::UNIX_EPOCH) {
+            params.push(("end".to_string(), duration.as_secs().to_string()));
+        } else {
+            return Err(PrometheusError{msg: format!("Error with time {:?}", end_time)});
+        }
+    }
+
+    let response  = client.get(&url).query(&params).send()?;
+    if !response.status().is_success() {
+        return Err(PrometheusError{msg : format!("Prometheus return API error code: {:?}", response.text())});
+    }
+
+    let prometheus_res: PrometheusResponse = response.json()?;
+    Ok(prometheus_res)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use crate::client::prometheus::{
 };
 
 fn main() {
-    run_prometheus("some query".to_string(), PrometheusQueryParams::default() , PrometheusConfig::default());
-    println!("Hello, world!");
+    let query: String = format!("histogram_quantile(0.99, sum by (le) (rate(bob_http_request_duration_seconds_bucket[10m])))");
+    let res = run_prometheus(query, PrometheusQueryParams::default() , PrometheusConfig::default());
+    println!("Hello, world! {:?}", res);
 }
